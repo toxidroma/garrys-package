fa33455fd6100f6775cd113e4793f1f2f9650767
witch <janeflowerss@protonmail.com>
2023-05-20 18:33:59 -0400

    oops

--- lua/packages/garrys-package/init.lua
+++ lua/packages/garrys-package/init.lua
@@ -1,4 +1,10 @@
-return hook.Add('Think', tostring(_PKG), function()
-  print("Garry's Package")
-  return nil
-end)
+local random
+random = math.random
+local int = 1 + 4
+return {
+  key1 = true,
+  key2 = 'Yes',
+  [false] = '0',
+  result = int + 3,
+  int = int
+}
